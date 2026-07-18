3183c7dcf584aeb2a8d3827ab3d362372b51b88e
Anthony Hagidimitriou <info@anthonyhagi.com.au>
2026-07-18 16:14:02 +0930

    fix(server): ensure copyright notice added to files

--- packages/server/tsdown.config.ts
+++ packages/server/tsdown.config.ts
@@ -1,6 +1,9 @@
 import { defineConfig } from "tsdown";
 
+import packageJson from "./package.json" with { type: "json" };
+
 export default defineConfig({
+  banner: `/*! ${packageJson.name} v${packageJson.version} | MIT License | Copyright (c) 2026 Anthony Con Hagidimitriou */`,
   entry: ["./src/index.ts"],
   outDir: "dist",
   platform: "node",
